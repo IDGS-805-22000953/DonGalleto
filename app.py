fb6ecf85c403c8094afcdffc40e09864c6bc1de5
IDGS-805-2200095 <79194@alumnos.utleon.edu.mx>
2025-03-09 15:59:45 -0600

    Don Galleto Base

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from flask import g
+from config import DevelopmentConfig
+
+from models import db
+
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf=CSRFProtect()
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+	return render_template("index.html")
+
+@app.route("/")
+@app.route("/central")
+def central():
+	return render_template("Central/inicioCentral.html")
+
+@app.route("/")
+@app.route("/cliente")
+def cliente():
+	return render_template("Cliente/inicioCliente.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+        return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    csrf.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run()
---
+++ forms.py
@@ -0,0 +1,8 @@
+from wtforms import Form
+from flask_wtf import FlaskForm
+ 
+from wtforms import StringField,IntegerField
+from wtforms import EmailField
+from wtforms import validators
+ 
+ 
